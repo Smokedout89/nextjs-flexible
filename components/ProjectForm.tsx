4eae2c3c63523fc248afdcd2362c5d6d97d08e89
Smokedout89 <smokedout8989@gmail.com>
2023-07-18 16:45:27 +0300

    Started working on creating new project

---
+++ components/ProjectForm.tsx
@@ -0,0 +1,14 @@
+import { SessionInterface } from "@/common.types"
+
+type Props = {
+  type: string,
+  session: SessionInterface
+}
+
+const ProjectForm = ({ type, session}: Props) => {
+  return (
+    <div>ProjectForm</div>
+  )
+}
+
+export default ProjectForm
