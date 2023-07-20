6c1968d0c63e0586826aa97df850c8356168cd28
Smokedout89 <smokedout8989@gmail.com>
2023-07-20 11:32:23 +0300

    Finished image uploading,  started project cards

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,5 @@
 import { ProjectForm } from "@/common.types";
-import { createProjectMutation, createUserMutation, getUserQuery } from "@/graphql";
+import { createProjectMutation, createUserMutation, getUserQuery, projectsQuery } from "@/graphql";
 import { GraphQLClient } from "graphql-request";
 
 const isProduction = process.env.NODE_ENV === "production";
@@ -41,7 +41,6 @@ export const fetchToken = async () => {
     const response = await fetch(`${serverUrl}/api/auth/token`);
     return response.json();
   } catch (error) {
-
     throw error;
   }
 }
@@ -78,4 +77,11 @@ export const createNewProject = async (form: ProjectForm,
 
       return makeGraphQLRequest(createProjectMutation, variables);
     }
+  }
+
+  export const fetchAllProjects = async (
+    category?: string, endcursor?: string) => {
+    client.setHeader("x-api-key", apiKey);
+    
+    return makeGraphQLRequest(projectsQuery, { category, endcursor})
   }
