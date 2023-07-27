3fd2cba870952edd1d776e1b0552cb40a9f32ea6
Smokedout89 <smokedout8989@gmail.com>
2023-07-27 13:48:45 +0300

    Implemented editing of project

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,7 +3,8 @@ import {
   createProjectMutation, createUserMutation,
   deleteProjectMutation,
   getProjectByIdQuery, getProjectsOfUserQuery, getUserQuery,
-  projectsQuery
+  projectsQuery,
+  updateProjectMutation
 } from "@/graphql";
 import { GraphQLClient } from "graphql-request";
 
@@ -106,4 +107,36 @@ export const getUserProjects = async (id: string, last?: number) => {
 export const deleteProject = async (id: string, token: string) => {
   client.setHeader("Authorization", `Bearer ${token}`);
   return makeGraphQLRequest(deleteProjectMutation, { id });
+}
+
+export const updateProject = async (
+  form: ProjectForm, projectId: string, token: string) => {
+
+  function isBase64DataURL(value: string) {
+    const base64Regex = /^data:image\/[a-z]+;base64,/;
+    return base64Regex.test(value);
+  }
+
+  let updatedForm = { ...form };
+
+  const isUploadingNewImage = isBase64DataURL(form.image);
+
+  if (isUploadingNewImage) {
+    const imageUrl = await uploadImage(form.image);
+
+    if (imageUrl.url) {
+      updatedForm = {
+        ...form,
+        image: imageUrl.url
+      }
+    }
+  }
+
+  const variables = {
+    id: projectId,
+    input: updatedForm
+  }
+
+  client.setHeader("Authorization", `Bearer ${token}`);
+  return makeGraphQLRequest(updateProjectMutation, variables);
 }
